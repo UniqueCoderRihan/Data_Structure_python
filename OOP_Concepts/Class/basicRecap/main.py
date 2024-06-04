689f4249a17b5decf0c8af55ae9199bf558511a9
raihanuldev <rihanthedev@gmail.com>
2024-06-05 00:56:32 +0600

    code updated

--- OOP_Concepts/Class/basicRecap/main.py
+++ OOP_Concepts/Class/basicRecap/main.py
@@ -1,4 +1,23 @@
+
 users = ["raihan","sharif","habib","Tanim","rashid","shariar"]
 
-for i in range(len(users)):
-    print(users[i])
+class raihan:
+    def __init__(self,roll,age,gf):
+        self.roll = roll
+        self.age = age
+        self.gf = gf
+    def call(self):
+        print(f"gf status",self.gf)
+
+print(raihan(12,23,"nai Bhai gf").call())
+users.append("NewUser")
+users.pop()
+users.append("Again User")
+users.sort()
+indexOFUser= users.index("raihan")
+print(indexOFUser)
+print(users)
+
+# for i in range(len(users)):
+#     print(users[i])
+
